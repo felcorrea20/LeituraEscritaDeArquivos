9f7244f79ebfedc534d4d6d58a1c54e4e22f8479
Felipe Corrêa <felipe.correa2@outlook.com>
2025-11-24 22:24:34 -0300

    Criado classes para leitura e escrita em arquivos txt

---
+++ src/EscritaMain.java
@@ -0,0 +1,29 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class EscritaMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do arquivo: ");
+        String nomeArquivo = scanner.nextLine();
+
+        System.out.print("Digite um texto para gravar em um arquivo: ");
+        String textoParaGravacao = scanner.nextLine();
+
+        try {
+            FileWriter escrita = new FileWriter(nomeArquivo + ".txt");
+            escrita.write(textoParaGravacao);
+            escrita.close();
+
+            System.out.println("Arquivo criado com sucesso!");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
---
+++ src/LeituraMain.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LeituraMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o arquivo para leitura: ");
+        String nomeArquivo = scanner.next();
+
+        scanner.close();
+
+        try {
+            File arquivoParaLeitura = new File(nomeArquivo + ".txt");
+            Scanner leitura = new Scanner(arquivoParaLeitura);
+
+            while (leitura.hasNextLine()) {
+                System.out.println(leitura.nextLine());
+            }
+
+            leitura.close();
+
+        } catch (FileNotFoundException e){
+            System.out.println("Arquivo nao encontrado!");
+        }
+
+    }
+
+}
